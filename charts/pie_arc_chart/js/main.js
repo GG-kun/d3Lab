2e551b67ab53faad0807760c6f1e49ce18d539bb
Guillermo González <0205838@up.edu.mx>
2021-03-15 09:53:28 -0600

    Feature: Exercise 12

--- charts/pie_arc_chart/js/main.js
+++ charts/pie_arc_chart/js/main.js
@@ -19,21 +19,25 @@ var g = svg.append("g")
 
 var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-// TODO: create the arc generator for a donut chart.
-// var arc = ...
+var arc = d3.arc()
+	.outerRadius(radius - 20)
+	.innerRadius(radius - 80);
 
-// TODO: create the pie layout generator.
-// var pie = ...
+var pie = d3.pie()
+	.value((d) => { return d.count; })
+	.sort(null);
 
 d3.tsv("data/donut2.tsv").then((data) => {
-    // TODO: Transform data to its proper format
-    // count -> number
-    // fruit -> lower case
+    data.forEach((d) => {
+        d.count = +d.count;    
+        d.fruit= d.fruit.toLowerCase();    
+    });
 
     console.log(data);
     
-    // TODO: create the nest function to group by fruits
-    var regionsByFruit = null;
+    var regionsByFruit = d3.nest()
+        .key((d) => { return d.fruit; })
+        .entries(data);
 
     console.log(regionsByFruit)
 
