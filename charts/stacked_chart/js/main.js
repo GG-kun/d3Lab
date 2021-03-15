ac9b0b11337e4b9c7e3806f08696bed6b1166aea
Guillermo González <0205838@up.edu.mx>
2021-03-15 09:42:24 -0600

    Feature: Exercise 11

--- charts/stacked_chart/js/main.js
+++ charts/stacked_chart/js/main.js
@@ -30,10 +30,15 @@ var xAxisCall = d3.axisBottom();
 var yAxisCall = d3.axisLeft().tickFormat(formatBillion);
 
 // Area generator
-// TODO: create the area generator. 
+var area = d3.area()
+    .x((d) => { 
+        return x(d.data.date);
+    })
+    .y0((d) => { return y(d[0]); })
+    .y1((d) => { return y(d[1]); });
 // The x coordinate will be the date of the data
 // while y0 and y1 will be in the 0 and 1 positions of the d element
-// TODO: create the stack
+var stack = d3.stack();
 
 // Axis groups
 var xAxis = g.append("g")
@@ -63,8 +68,7 @@ d3.csv('data/stacked_area2.csv').then((data) => {
         return key !== 'date'; 
     }));
         
-    //TODO: obtain the keys array, remember to remove the first column
-    var keys = [];
+    var keys = data.columns.filter((key) => { return key !== 'date'; });
 
 	data.forEach((d) => {
 	    d.date = parseDate(d.date);
@@ -85,16 +89,38 @@ d3.csv('data/stacked_area2.csv').then((data) => {
     yAxis.call(yAxisCall.scale(y))
 
     // Add stacked area chart
-    // TODO: finish the configuration of the stack object
-    // by setting the keys, order and offset
+    stack.keys(keys)
+        .order(d3.stackOrderNone)
+        .offset(d3.stackOffsetNone);
 
-    // TODO: bind the data to the stack and create the group 
-    // that will contain the area path
+    var browser = g.selectAll('.browser')
+        .data(stack(data))        
+        .enter().append('g')        
+        .attr('class', (d) => { return 'browser ' + d.key; })        
+        .attr('fill-opacity', 0.5);
 
-    // TODO: call the area generator with the appropriate data
-
+    browser.append('path')
+        .attr('class', 'area')
+        .attr('d', area)
+        .style('fill', (d) => { return color(d.key); });
     // Create legend
-    // TODO: Create a legend showing all the names of every color
+    var legend = g.append("g")
+    .attr("transform", "translate(" + (width+150) + "," + (height - 210) + ")");
+
+    keys.reverse().forEach((country, i) => {
+    var legendRow = legend.append("g")
+        .attr("transform", "translate(0, " + (i * 20) + ")");
+    legendRow.append("rect")
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", color(country));
+    legendRow.append("text")    
+        .attr("x", -10)    
+        .attr("y", 10)    
+        .attr("text-anchor", "end")    
+        .style("text-transform", "capitalize")    
+        .text(country);    
+    });
 
 }).catch((error) => {
     console.log(error);
